56c30301a1c9aefe80e2311ce721d648bea29ecc
Michael Katz <michael.katz1@ibm.com>
2025-01-17 17:32:17 -0500

    causal graph dump to dot

---
+++ get_cg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# $1 - domain file
+# $2 - problem file
+
+./fast-downward.py --keep-sas-file $1 $2 --search "astar(max([cg(),const(value=infinity)]))" > /dev/null
+
+dot -Tpdf causal_graph.dot > causal_graph.pdf
