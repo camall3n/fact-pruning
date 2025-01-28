d6aef886ccd553a01dc5e460a8e4a6336a8dbb04
Cam Allen <camallen@berkeley.edu>
2025-01-27 16:42:11 -0800

    Add run-scoping.sh script

---
+++ run-scoping.sh
@@ -0,0 +1,4 @@
+DOMAIN=$1
+PROBLEM=$2
+shift 2
+./build.py && PYTHONPATH=./builds/release/bin/ ./fast-downward.py $DOMAIN $PROBLEM --search "astar(lmcut())" --translate-options --keep-unimportant-variables $@
