02df8808b01591ee5551d0e6367be1c6ec6f0b2c
Cam Allen <camallen@berkeley.edu>
2025-01-27 16:41:52 -0800

    Add PYTHONPATH fix for get_cg.sh script

--- get_cg.sh
+++ get_cg.sh
@@ -2,6 +2,6 @@
 # $1 - domain file
 # $2 - problem file
 
-./fast-downward.py --keep-sas-file $1 $2 --search "astar(max([cg(),const(value=infinity)]))" > /dev/null
+PYTHONPATH=./builds/release/bin/ ./fast-downward.py --keep-sas-file $1 $2 --search "astar(max([cg(),const(value=infinity)]))" > /dev/null
 
 dot -Tpdf causal_graph.dot > causal_graph.pdf
